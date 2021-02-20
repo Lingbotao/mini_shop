55b96f4b47fdfb0c429ff3384009b74d971eae33
Lingbotao <1042793969@qq.com>
2021-02-20 22:59:56 +0800

    server中加入处理跨域问题，连接本地数据库，微信登陆和支付，公开操作访问内容，收货地址信息利用，用户数据操作数据等功能

---
+++ server/src/common/config/datebase.js
@@ -0,0 +1,14 @@
+const mysql = require("think-model-mysql");
+
+//连接本地数据库
+module.exports = {
+  handle: mysql,
+  database: "shop",
+  prefix: "shop_",
+  encoding: "utf8mb4",
+  host: "127.0.0.1",
+  port: "3306",
+  user: "root",
+  password: "123456",
+  dateStrings: true,
+};
